13dad40f1caf22236a9b0d065649db320558002e
bessaa.amirouche <bessaa.amirouche@outlook.fr>
2023-01-28 23:29:42 +0100

    logo

--- src/app/personne-presentes/utilisateur.ts
+++ src/app/personne-presentes/utilisateur.ts
@@ -5,67 +5,67 @@ export const Utilisateur: CardProfil[] = [
     nom: "bessaa",
     prenom: "amir",
     etat: "connecté",
-    photo: '/assets/genie.jpg'
+    photo: '/assets/images/genie.jpg'
 },
 {
     nom: "bessaa",
     prenom: "oussama",
     etat: "connecté",
-    photo: '/assets/genie.jpg'
+    photo: '/assets/images/genie.jpg'
 },
 {
-    nom: "bessaa",
+    nom: "Mehidi",
     prenom: "lamine",
     etat: "connecté",
-    photo: '/assets/genie.jpg'
+    photo: '/assets/images/genie.jpg'
 },
 {
     nom: "bessaa",
     prenom: "amir",
     etat: "connecté",
-    photo: '/assets/genie.jpg'
+    photo: '/assets/images/genie.jpg'
 },
 {
     nom: "bessaa",
     prenom: "onessai",
     etat: "connecté",
-    photo: '/assets/genie.jpg'
+    photo: '/assets/images/genie.jpg'
 },
 {
     nom: "bessaa",
     prenom: "etonarrive",
     etat: "connecté",
-    photo: '/assets/genie.jpg'
+    photo: '/assets/images/genie.jpg'
 },
 {
     nom: "bessaa",
     prenom: "attendez",
     etat: "connecté",
-    photo: '/assets/genie.jpg'
+    photo: '/assets/images/genie.jpg'
 },
 {
     nom: "bessaa",
     prenom: "paslongtemp",
     etat: "connecté",
-    photo: '/assets/genie.jpg'
+    photo: '/assets/images/genie.jpg'
 },
 {
     nom: "bessaa",
     prenom: "youhou",
     etat: "connecté",
-    photo: '/assets/genie.jpg'
+    photo: '/assets/images/genie.jpg'
 },
 {
     nom: "bessaa",
     prenom: "amir",
     etat: "connecté",
-    photo: '/assets/genie.jpg'
+    photo: '/assets/images/genie.jpg'
 },
 {
     nom: "bessaa",
     prenom: "amir",
     etat: "connecté",
-    photo: '/assets/genie.jpg'
+    photo: '/assets/images/genie.jpg'
 },
 
 ]
